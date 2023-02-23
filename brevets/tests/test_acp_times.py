4330ac86fa963b92f92d5b0cfcbfb5f6e64227f5
Deihman <clownvant@icloud.com>
2023-02-22 17:15:37 -0800

    calc.html ERROR

--- brevets/tests/test_acp_times.py
+++ brevets/tests/test_acp_times.py
@@ -6,6 +6,8 @@ Write your tests HERE AND ONLY HERE.
 
 import nose    # Testing framework
 import logging
+import ../acp_times.py
+
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=logging.WARNING)
 log = logging.getLogger(__name__)
